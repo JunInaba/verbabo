9b5b21a3256c1ec4e3c801e5b3c2ca02c977e042
InabaJun <inabajun@InabaJun-no-Mac-mini.local>
2015-04-19 22:58:14 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Verbabo::Application.config.secret_key_base = '279138eb75e67eb84870f8be519ac6dc019a220867e5c0ff7da1b8685f034b6a2150978d19d5e82dc3c03f00c5f16ae8fc245ccffb6d78e14122126836de1eb0'
